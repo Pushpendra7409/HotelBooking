414c3761d4ee335d70f1b3c98434c6416e26e2c4
Pushpendra7409 <pushpendra7409@gmail.com>
2024-07-13 11:15:09 +0530

    Add show Route, Rupee Symbol,toLocale(en-IN) for commas

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/",  (req, res) => {
     res.send("Hi, I am root");
@@ -30,6 +31,13 @@ app.get("/listings", async (req, res) => {
    const allListings = await Listing.find({});
        res.render("./listings/index.ejs", { allListings });
 });
+
+// SHOW Route
+app.get("/listings/:id", async (req, res) => {
+   let {id} = req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/show.ejs", { listing });
+})
 // app.get("/testListing", async (req, res) =>{
 //   let sampleListing = new Listing({
 //      title: "My New Villa",
