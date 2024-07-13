2ec096f15e842bec938365f89515d1f026f3b1b0
Pushpendra7409 <pushpendra7409@gmail.com>
2024-07-13 13:53:50 +0530

    Edit and Update Route in it

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airBnb";
@@ -21,6 +22,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/",  (req, res) => {
     res.send("Hi, I am root");
@@ -55,6 +57,19 @@ app.post("/listings", async (req, res) =>{
     res.redirect("/listings");
 });
 
+//EDIT Route
+app.get("/listings/:id/edit", async (req,res) =>{
+   let {id} = req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/edit.ejs", { listing });
+});
+
+//UPDATE Route
+app.put("/listings/:id", async (req, res) =>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
 
 
 // app.get("/testListing", async (req, res) =>{
